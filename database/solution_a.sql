40db2b972d81bd8a9ffca169e346f8162959474c
bengoan3012 <huynhthibengoan.tdc2223@gmail.com>
2025-03-25 22:14:45 +0700

    Update solution_a.sql

--- database/solution_a.sql
+++ database/solution_a.sql
@@ -1,1 +1,27 @@
+-- 1. Lấy danh sách người dùng theo thứ tự tên theo Alphabet (A->Z)
+SELECT * FROM users ORDER BY user_name ASC;
+
+-- 2. Lấy 7 người dùng theo thứ tự tên Alphabet (A->Z)
+SELECT * FROM users ORDER BY user_name ASC LIMIT 7;
+
+-- 3. Lấy danh sách người dùng theo thứ tự tên Alphabet (A->Z), có chứa chữ 'a'
+SELECT * FROM users WHERE user_name LIKE '%a%' ORDER BY user_name ASC;
+
+-- 4. Lấy danh sách người dùng có tên bắt đầu bằng chữ 'm'
+SELECT * FROM users WHERE user_name LIKE 'm%';
+
+-- 5. Lấy danh sách người dùng có tên kết thúc bằng chữ 'i'
+SELECT * FROM users WHERE user_name LIKE '%i';
+
+-- 6. Lấy danh sách người dùng có email thuộc Gmail
+SELECT * FROM users WHERE user_email LIKE '%@gmail.com';
+
+-- 7. Lấy danh sách người dùng có email Gmail và bắt đầu bằng chữ 'm'
+SELECT * FROM users WHERE user_email LIKE '%@gmail.com' AND user_name LIKE 'm%';
+
+-- 8. Lấy danh sách người dùng có email Gmail, tên chứa chữ 'i' và độ dài tên > 5
+SELECT * FROM users WHERE user_email LIKE '%@gmail.com' AND user_name LIKE '%i%' AND LENGTH(user_name) > 5;
+
+-- 9. Lấy danh sách người dùng có email Gmail, tên có chữ 'i' và chiều dài từ 5 đến 9
+SELECT * FROM users WHERE user_email LIKE '%@gmail.com' AND user_name LIKE '%i%' AND LENGTH(user_name) BETWEEN 5 AND 9;
 
